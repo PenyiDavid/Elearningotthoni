fc9dce89a30ae9e9523a1a7ec1c69a31bd233f9f
PenyiDavid <penyaskodavid@gmail.com>
2024-10-12 21:49:46 +0200

    Kapcsolat a subject és question között

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -10,4 +10,8 @@ class Question extends Model
 {
     use HasFactory, SoftDeletes;
     protected $fillable = ['question_text', 'subject_id'];
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
 }
